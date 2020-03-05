3027555b0e99038c6874cde58f76ea55c7c4aa5c
Rodrigo carvalho barbosa <rodrigocarvalhobarbosa@hotmail.com>
2020-03-05 05:49:40 -0300

    Add project files.

---
+++ Prova_cria/cadastrarCliente.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Prova_cria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_nomeCliente = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.dataAluguel = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDiasAluguel = new System.Windows.Forms.TextBox();
+            this.lblAlguel = new System.Windows.Forms.Label();
+            this.ckCartao = new System.Windows.Forms.CheckBox();
+            this.btnCadastro = new System.Windows.Forms.Button();
+            this.btnDias = new System.Windows.Forms.Button();
+            this.diasAluguel = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.qtdPessoas = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.qtdPessoas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_nomeCliente
+            // 
+            this.lbl_nomeCliente.AutoSize = true;
+            this.lbl_nomeCliente.Location = new System.Drawing.Point(13, 12);
+            this.lbl_nomeCliente.Name = "lbl_nomeCliente";
+            this.lbl_nomeCliente.Size = new System.Drawing.Size(94, 13);
+            this.lbl_nomeCliente.TabIndex = 0;
+            this.lbl_nomeCliente.Text = "Nome do Cliente : ";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(132, 5);
+            this.txtNome.MaxLength = 34;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(213, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // dataAluguel
+            // 
+            this.dataAluguel.CustomFormat = "dd/mm/yyyy";
+            this.dataAluguel.Location = new System.Drawing.Point(132, 83);
+            this.dataAluguel.Name = "dataAluguel";
+            this.dataAluguel.Size = new System.Drawing.Size(213, 20);
+            this.dataAluguel.TabIndex = 5;
+            this.dataAluguel.Value = new System.DateTime(2020, 3, 4, 13, 47, 44, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Primeiro dia do Aluguel:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Dias do Aluguel:";
+            // 
+            // txtDiasAluguel
+            // 
+            this.txtDiasAluguel.Location = new System.Drawing.Point(132, 57);
+            this.txtDiasAluguel.Name = "txtDiasAluguel";
+            this.txtDiasAluguel.Size = new System.Drawing.Size(213, 20);
+            this.txtDiasAluguel.TabIndex = 4;
+            this.txtDiasAluguel.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDiasAluguel_KeyPress);
+            // 
+            // lblAlguel
+            // 
+            this.lblAlguel.AutoSize = true;
+            this.lblAlguel.Location = new System.Drawing.Point(13, 109);
+            this.lblAlguel.Name = "lblAlguel";
+            this.lblAlguel.Size = new System.Drawing.Size(91, 13);
+            this.lblAlguel.TabIndex = 9;
+            this.lblAlguel.Text = "Datas do Aluguel:";
+            // 
+            // ckCartao
+            // 
+            this.ckCartao.AutoSize = true;
+            this.ckCartao.Location = new System.Drawing.Point(351, 5);
+            this.ckCartao.Name = "ckCartao";
+            this.ckCartao.Size = new System.Drawing.Size(108, 17);
+            this.ckCartao.TabIndex = 2;
+            this.ckCartao.Text = "Cartão Fidelidade";
+            this.ckCartao.UseVisualStyleBackColor = true;
+            // 
+            // btnCadastro
+            // 
+            this.btnCadastro.Location = new System.Drawing.Point(175, 184);
+            this.btnCadastro.Name = "btnCadastro";
+            this.btnCadastro.Size = new System.Drawing.Size(123, 23);
+            this.btnCadastro.TabIndex = 7;
+            this.btnCadastro.Text = "Cadastrar Cliente";
+            this.btnCadastro.UseVisualStyleBackColor = true;
+            this.btnCadastro.Click += new System.EventHandler(this.btnCadastro_Click);
+            // 
+            // btnDias
+            // 
+            this.btnDias.Location = new System.Drawing.Point(351, 83);
+            this.btnDias.Name = "btnDias";
+            this.btnDias.Size = new System.Drawing.Size(101, 23);
+            this.btnDias.TabIndex = 6;
+            this.btnDias.Text = "Total de Dias ";
+            this.btnDias.UseVisualStyleBackColor = true;
+            this.btnDias.Click += new System.EventHandler(this.btnDias_Click);
+            // 
+            // diasAluguel
+            // 
+            this.diasAluguel.FormattingEnabled = true;
+            this.diasAluguel.Location = new System.Drawing.Point(132, 109);
+            this.diasAluguel.Name = "diasAluguel";
+            this.diasAluguel.Size = new System.Drawing.Size(213, 69);
+            this.diasAluguel.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Qtd de Pessoas: ";
+            // 
+            // qtdPessoas
+            // 
+            this.qtdPessoas.Location = new System.Drawing.Point(132, 31);
+            this.qtdPessoas.Maximum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.qtdPessoas.Name = "qtdPessoas";
+            this.qtdPessoas.Size = new System.Drawing.Size(213, 20);
+            this.qtdPessoas.TabIndex = 12;
+            this.qtdPessoas.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 219);
+            this.Controls.Add(this.qtdPessoas);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.diasAluguel);
+            this.Controls.Add(this.btnDias);
+            this.Controls.Add(this.btnCadastro);
+            this.Controls.Add(this.lblAlguel);
+            this.Controls.Add(this.txtDiasAluguel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataAluguel);
+            this.Controls.Add(this.ckCartao);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lbl_nomeCliente);
+            this.Name = "Form1";
+            this.Text = "Cadastar Clientes";
+            ((System.ComponentModel.ISupportInitialize)(this.qtdPessoas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_nomeCliente;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.DateTimePicker dataAluguel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDiasAluguel;
+        private System.Windows.Forms.Label lblAlguel;
+        private System.Windows.Forms.CheckBox ckCartao;
+        private System.Windows.Forms.Button btnCadastro;
+        private System.Windows.Forms.Button btnDias;
+        private System.Windows.Forms.ListBox diasAluguel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown qtdPessoas;
+    }
+}
+
---
+++ Prova_cria/cadastrarCliente.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Prova_cria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] totalDias;
+
+        private void txtDiasAluguel_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && !(e.KeyChar == (char)Keys.Enter) && !(e.KeyChar == (char)Keys.Back))
+                {
+                e.Handled = true;
+                MessageBox.Show("Este campo aceita somente número");
+            }
+        }
+
+        private void btnCadastro_Click(object sender, EventArgs e)
+        {
+            if (diasAluguel.Items.Count == 0)
+            {
+                MessageBox.Show("É preciso clicar no botão 'Total de Dias' antes");
+            }
+            else
+            { 
+                int tVetor = totalDias.Length;
+                FolderBrowserDialog dialog = new FolderBrowserDialog();
+                dialog.Description = "Selecione a pasta onde será criado o arquivo:";
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    ////declarando a variavel do tipo StreamWriter para 
+                    //abrir ou criar um arquivo para escrita 
+                    StreamWriter x;
+
+                    //utilizando o metodo para criar um arquivo texto
+                    //e associando o caminho e nome ao metodo
+                    x = File.CreateText(System.IO.Path.Combine(dialog.SelectedPath, txtNome.Text + ".txt"));
+
+                    //escrevendo o titulo
+                    x.WriteLine("            Dados do Cliente" + " " + txtNome.Text);
+                    //pulando linha sem escrita
+                    x.WriteLine();
+                    //escrevendo conteúdo do recibo
+                    x.WriteLine("Nome do Cliente: " + txtNome.Text);
+                    x.WriteLine("===========================================");
+                    if (ckCartao.Checked == true)
+                    {
+                        x.WriteLine("Cartão Fidelidade: Sim");
+                    }
+                    else
+                    {
+                        x.WriteLine("Cartão Fidelidade: Não");
+                    }
+                    x.WriteLine("===========================================");
+                    x.WriteLine("Quantidade de Pessoas: " + qtdPessoas.Text);
+                    x.WriteLine("===========================================");
+                    x.WriteLine("Total de dias do Aluguel: " + tVetor);
+                    x.WriteLine("===========================================");
+                    x.WriteLine("Dias do Aluguel  Dia da Semana do Aluguel");
+                    for (int i = 0; i < tVetor; i++)
+                    {
+                        x.WriteLine(totalDias[i]);
+                    }
+
+                    //fechando o arquivo texto com o método .Close()
+                    x.Close();
+
+                    Application.Exit();
+                }            
+            }
+        }
+
+        private void btnDias_Click(object sender, EventArgs e)
+        {
+            if (txtDiasAluguel.Text == "")
+            {
+                MessageBox.Show("Você precisa colocar a quantidade de dias do aluguel");
+            }
+            else
+            {
+                int dias = int.Parse(txtDiasAluguel.Text);
+                string data, semana, aluguel;
+                dias = dias - 1;
+                DateTime inicio = dataAluguel.Value;
+                totalDias = new string[dias + 1];
+
+                for (int i = 0; i <= dias; i++)
+                {
+                    semana = inicio.AddDays(i).DayOfWeek.ToString();
+
+                    switch (semana)
+                    {
+                        case "Monday":
+                            semana = "Segunda";
+                            break;
+                        case "Tuesday":
+                            semana = "Terça";
+                            break;
+                        case "Wednesday":
+                            semana = "Quarta";
+                            break;
+                        case "Thursday":
+                            semana = "Quinta";
+                            break;
+                        case "Friday":
+                            semana = "Sexta";
+                            break;
+                        case "Saturday":
+                            semana = "Sábado";
+                            break;
+                        case "Sunday":
+                            semana = "Domingo";
+                            break;
+                    };
+
+                    data = "Data: " + inicio.AddDays(i).ToString().Substring(0, 10);
+                    semana = "Semana: " + semana;
+                    aluguel = data + " " + semana;
+                    diasAluguel.Items.Add(data + " " + semana);
+                    totalDias[i] = aluguel;
+                }
+            }
+        }
+
+        private void txtQtdPessoas_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && !(e.KeyChar == (char)Keys.Enter) && !(e.KeyChar == (char)Keys.Back))
+            {
+                e.Handled = true;
+                MessageBox.Show("Este campo aceita somente numero");
+            }
+        }
+
+    }
+}
